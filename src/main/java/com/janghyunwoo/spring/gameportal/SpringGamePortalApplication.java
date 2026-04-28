9b5952999d20db868b78cfd0f128cd9945305d3c
gusdn6763 <gusdn6763@gmail.com>
2026-04-28 11:51:50 +0900

    init: 초기 세팅 설정
    1. gitignore 설정.
    2. 의존성 추가. -> 향후 더 추가 예정.
    3. 정상 작동 확인

---
+++ src/main/java/com/janghyunwoo/spring/gameportal/SpringGamePortalApplication.java
@@ -0,0 +1,13 @@
+package com.janghyunwoo.spring.gameportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGamePortalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringGamePortalApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/janghyunwoo/spring/gameportal/SpringGamePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janghyunwoo.spring.gameportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGamePortalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
